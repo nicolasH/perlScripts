b612dc95eae7bc9070147e0d5df647b14cebced0
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2010-11-01 00:42:30 +0100

    is now a webservice!

--- coordinates/index.cgi
+++ coordinates/index.cgi
@@ -1,22 +1,47 @@
 #!/usr/bin/perl -w
-use CGI qw/:standard/;
+use CGI qw/:standard canvas/;
 use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
 use strict;
+use DBI;
 use DBD::SQLite;
 
+my $dbfile = "prevessin.db";
 
+my @result;
+my $building = param('buildingName');
+
+if(param){
+	print header('application/json');
+	my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","","");
+	#currently exact match only
+	my $sth = $dbh->prepare('select file,word, x1, y1, x2, y2 from WORDS where word like ?');
+	$sth->execute($building.'%');
+	$sth->bind_columns(\my($file,$word,$x1,$y1,$x2,$y2));
+	print "[";
+	while (my@data = $sth->fetchrow_array()) {
+    	if(defined $word){
+	   		print '{"word" : "'.$word.'", "x1" : '.$x1.', "y1": '.$y1.', "x2":'.$x2.',"y2":'.$y2.'},';
+    	}
+	}
+	print ']';
+  	$sth->finish;
+	undef($dbh);
+	print footer;
+}else{
 
 print 
        header,
-       start_html('Building Locator'),
-       h1('Get the Prevessin Buildings Locations'),
-       start_form,
-       "Which building ?",textfield('building'),p
-       submit,
+       start_html(-title=> 'Building Locator', 
+       			-base => 'true', 
+       			-onload => 'draw()', 
+       			-script => { -language => 'javascript', -src => 'markers.js' }),
+       h2('Get the Location of the Prevessin Buildings'),
+       p,"A Perl + Ajax experiment",
+       start_form(-action=>''),
+       "Which building ? (press 'locate', not enter)",textfield(-name=>'building',-id=>'buildingName'),
+       button(-value=>'Locate',-onClick=>'asyncBuilding()'),
        end_form,
        hr,"\n";
-    if (param) {
-       print "You are looking for building ",em(param('building')),p,"\n";
-       
-    }
+    print canvas({-id=>'canvas',-width=>1589,-height=>1124});
     print end_html;
+}
---
+++ coordinates/markers.js
@@ -0,0 +1,80 @@
+function getRequest(){
+	var xhr; 
+   	try {  
+   		xhr = new ActiveXObject('Msxml2.XMLHTTP');
+   	}catch (e) {
+       	try {   
+       		xhr = new ActiveXObject('Microsoft.XMLHTTP');
+       	}catch (e2) {
+			try {  
+				xhr = new XMLHttpRequest();
+       		}catch (e3) {  xhr = false;   }
+       	}
+   	}
+   	return xhr;
+}
+
+
+function asyncBuilding(){
+	var http = getRequest();
+
+	http.onreadystatechange  = function(){ 
+		if(http.readyState  == 4){
+       		if(http.status  == 200) {
+   	         	results = eval('(' + http.responseText + ')');
+   	         	if(results.length>0){
+   	         	  var ctx = document.getElementById('canvas').getContext('2d');
+    			  ctx.drawImage(document.prevessin,0,0);
+   	         	}
+   	         	for(var i = 0; i<results.length; i++){
+   					var loc = results[i];
+   					if(loc != undefined && loc.word != undefined){
+	   					drawMarker(loc.word,loc.x1,loc.y1,loc.x2,loc.y2);
+	   				}
+   				}
+   	         	node = document.createElement('li');
+   				//node.innerHTML = li.content;
+   				//node.id = li.id;
+   				//document.getElementById("list_enlever").appendChild(node);
+       	    } else {
+	   	         document.getElementById('ajax').value="Error code " + http.status + " " +http.responseText;
+        	    }
+        	 }
+    	};
+		var test = document.getElementById("buildingName").value;
+ 		var url="buildingName="+test;
+ 		http.open("POST", "",  true);
+   		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		http.setRequestHeader("Content-length", url.length);
+		http.setRequestHeader("Connection", "close");
+   		http.send(url);
+	}
+
+
+
+function draw() {
+	var ctx = document.getElementById('canvas').getContext('2d');
+    document.prevessin = new Image();
+    document.prevessin.onload = function(){
+      ctx.drawImage(document.prevessin,0,0);
+    }
+    document.prevessin.src = 'prevessin.png';
+}
+ 
+function drawMarker(word,x1,y1,x2,y2){
+  	var ctx = document.getElementById('canvas').getContext('2d');
+    ctx.strokeStyle = 'darkorange';
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.moveTo(0,0);
+    y1 = (1124 - y1*1.3333  );  
+    x1 = x1 * 1.3333;
+    y2 = (1124 - y2*1.3333  );
+    x2 = x2 * 1.3333;
+    ctx.fillText(word, x1+3,12);
+    ctx.moveTo(x1,0);
+    ctx.lineTo(x1,y1);
+    ctx.lineTo(0,y1);
+    ctx.stroke();
+    
+  }
